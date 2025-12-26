963534a2fd2813980f04414feb88b0863361c7b8
AtharvNiprul74 <atharvniprul74@gmail.com>
2025-12-26 15:47:52 +0530

    Admin Courses API's Done.

--- Backend-Server/server.js
+++ Backend-Server/server.js
@@ -4,9 +4,13 @@ const app = express(); //This line creates an Express application object.
 //This line imports the public routes module and stores it in publicRouter.
 const publicRouter = require("./Routes/Public");
 
+//This line imports the Admin routes module and stores it in adminRouter.
+const adminRouter = require("./Routes/Admin");
+
 app.use(express.json());
 
 app.use("/public", publicRouter);
+app.use("/admin",  adminRouter);
 
 app.listen(4000, () => {
   console.log("Server started on port 4000 .......");
